72b2330b19faa9bee72dfccc8713adedf4b277bd
aryan008 <adam.s.ryan@pwc.com>
2021-09-21 16:48:05 +0000

    JS to line 114

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,8 +10,28 @@ for (i=0; i < updateBtns.length; i++) {
         if (user=="AnonymousUser") {
             console.log("User is not authenticated")
         } else {
-            console.log("Authenticated user...sending data")
+            updateUserOrder(productId, action)
         }
 
     })
+}
+
+function updateUserOrder(productId, action) {
+    console.log("User authenticated, sending data...")
+
+    var url = '/update_item/'
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrftoken,
+        },
+        body: JSON.stringify({"productId": productId, "action":action})
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        location.reload()
+    });
 }
