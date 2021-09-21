2707c6d0822ac82cf8bad1de36de479b2b2a1512
aryan008 <adam.s.ryan@pwc.com>
2021-09-21 16:35:58 +0000

    Start of JS

---
+++ static/js/cart.js
@@ -0,0 +1,17 @@
+var updateBtns = document.getElementsByClassName("update-cart")
+
+for (i=0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener("click", function() {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log("ProductId: ", productId, "Action: ", action)
+        console.log("User: ", user)
+
+        if (user=="AnonymousUser") {
+            console.log("User is not authenticated")
+        } else {
+            console.log("Authenticated user...sending data")
+        }
+
+    })
+}
